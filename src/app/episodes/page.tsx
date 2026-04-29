4fde9429f11af285b3427230e0d0cb47e8ff2437
maincharactermoney <maincharactermoney@gmail.com>
2026-04-28 20:07:48 -0700

    Update page.tsx

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -1,4 +1,6 @@
-tsimport "./episodes.css";  // on the list page
+ts
+
+import "./episodes.css";  // on the list page
 import "../episodes.css"; // on the [slug]/page.tsx (one level up)
 
 import Link from "next/link";
